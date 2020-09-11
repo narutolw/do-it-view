6bc84d4a51c9a249abcd96fc1f6c03ff46bcbf52
narutolw <2397401628@qq.com>
2020-09-12 01:39:45 +0800

    项目初建

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Axios from 'axios'
+
+Vue.config.productionTip = false
+
+// 不需要每次都 import一下 axios了，直接使用 $axios 即可
+Vue.prototype.$axios = Axios
+
+// step3：使每次请求都会带一个 /api 前缀
+Axios.defaults.baseURL = '/api'
+
+/* eslint-disable no-new */
+new Vue({
+  // el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
